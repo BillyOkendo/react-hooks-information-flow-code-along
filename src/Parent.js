f3755c18809ef87d933b3d2ae3c3aca628defdf7
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-07-20 00:05:23 +0300

    updated child.js and Parent.js

--- src/Parent.js
+++ src/Parent.js
@@ -6,10 +6,17 @@ function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setChildrenColor(newRandomColor); 
+  }
+
+
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
